549380e57993c44def629ff803189ef7cd62e72f
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-02-04 13:49:29 +0000

    test: persistence of user details

--- src/components/Dashboard.test.js
+++ src/components/Dashboard.test.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import { Dashboard } from './Dashboard'
 import { shallow } from 'enzyme'
+import { QUERY_MYPROFILE } from '../queries'
 
 describe(`<Dashboard />`, () => {
+  describe(`given query error`, () => {
+    const props = { data: { error: { message: '404' } } }
+    it(`should render Error`, () => {
+      const wrapper = shallow(<Dashboard {...props} />)
+      expect(wrapper.find('#error')).toHaveLength(1)
+    })
+  })
 
   describe('given getMyProfile query fetched ok', () => {
-    const props = { data: { getMyProfile: { username: 'Kunal', email: 'kunal.v.mandalia@gmail.com' }, loading: false }}
+    const mockMutate = () => new Promise((resolve, reject) => {
+      resolve({ data: { updateUsername: { token: 'dummy_token' }}})
+    })
+    const props = {
+      data: {
+        getMyProfile: {
+          username: 'Kunal',
+          email: 'kunal.v.mandalia@gmail.com'
+        },
+        loading: false
+      },
+      mutate: mockMutate
+    }
     it(`should allow user to update username`, () => {
       const newUsername = 'Galois'
       const wrapper = shallow(<Dashboard {...props} />)
@@ -13,13 +33,13 @@ describe(`<Dashboard />`, () => {
       wrapper.find('#new-username').simulate('change', {target: {value: newUsername}})
       expect(wrapper.state().newUsername).toEqual(newUsername)
     })
-  })
 
-  describe(`given query error`, () => {
-    const props = { data: { error: { message: '404' } } }
-    it(`should render Error`, () => {
+    it(`should persist new username in localStorage`, async () => {
+      const newUsername = 'Galois'
       const wrapper = shallow(<Dashboard {...props} />)
-      expect(wrapper.find('#error')).toHaveLength(1)
+      wrapper.find('#new-username').simulate('change', {target: {value: newUsername}})
+      await wrapper.find('#save-user-details').simulate('click')
+      expect(localStorage.setItem).toBeCalledWith('token', 'dummy_token')
     })
   })
 })
